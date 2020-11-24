bfdaf19df5fff3d75067366e98b81ea819296093
basikdimos <basic_dm@mail.ru>
2020-11-24 13:13:28 +0300

    Создан скилет модуля Tennis Manager

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Tennis Manger',
+    'version': '0.14.0.1',
+    'summary': 'Tennis Manger 2021',
+    'description': 'Help for tennis manager',
+    'category': 'sport',
+    'author': 'Basik Dmitry',
+    'website': 'no web site',
+    'depends': [
+        'base',
+    ],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'sequence': -100,
+}
